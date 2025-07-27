961cc51f53336c800bfbb1aa31e70607bfd2ab9f
JNVCPRASAD1507 <chandraprasadkolli@gmail.com>
2025-07-27 20:30:37 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+  import React from "react";
+  import { createRoot } from "react-dom/client";
+  import { BrowserRouter } from "react-router-dom";
+  import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter
-       future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true
-      }}
-    >
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+  createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true
+        }}
+      >
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
