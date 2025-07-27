8ddee21cb0316ab160ff39409a6585669e67c8cf
JNVCPRASAD1507 <chandraprasadkolli@gmail.com>
2025-07-27 22:04:34 +0530

    first commit

--- src/Components/marquee.jsx
+++ src/Components/marquee.jsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import React from "react";
 
-function MarqueeBanner() {
+const MarqueeBanner = () => {
   return (
     <>
       <Box>
